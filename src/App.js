aaf2c800be9217cb107752b7fa2331d0cd5df15a
Mr-Wii <erubakugeki@gmail.com>
2020-01-22 01:21:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Contacts from './components/contacts'
+class App extends Component {
+  state = {
+    contacts: []
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    fetch(
+      'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/shyuki',
+      {
+        method: 'GET',
+        headers: {
+          'X-Riot-Token': process.env.RIOT_LOL_API_KEY
+        }
+      }
+    )
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ contacts: data })
+        console.log(this.state)
+      })
+      .catch(console.log)
+  }
+
+  render() {
+    return <Contacts contacts={this.state.contacts} />
+  }
 }
 
-export default App;
+export default App
